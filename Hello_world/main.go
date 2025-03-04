d46dbbd7ca183a89a06c4b8e158ee4da22bfe188
tikrack <tikrackcode@gmail.com>
2025-03-04 20:53:42 +0330

    feat(handle): handle add value inoine two var

--- Hello_world/main.go
+++ Hello_world/main.go
@@ -3,20 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	x := 100
+	x, y := 5, 6
 
-	swich(x)
-}
-
-func swich(x int) {
-	switch x {
-	case 100:
-		fmt.Println("x is 100")
-		break
-	case 200:
-		fmt.Println("x equal to 200")
-		break
-	default:
-		fmt.Println("swich fail")
-	}
+	fmt.Println(x, y)
 }
