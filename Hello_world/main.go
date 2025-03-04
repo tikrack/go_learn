99b7cca236d339dfcef8c1f937e790486f9897ba
tikrack <tikrackcode@gmail.com>
2025-03-04 20:17:20 +0330

    feat(use): use easy mode

--- Hello_world/main.go
+++ Hello_world/main.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var number int
-	number = 100
+	//var number int
+	//number = 100
 
-	var float float64
-	float = float64(number)
+	number := 100
+
+	//var float float64
+	//float = float64(number)
+
+	float := float64(100)
 
 	fmt.Printf("value: %v and Type: %T \n", number, number)
 	fmt.Printf("value: %v and Type: %T \n", float, float)
