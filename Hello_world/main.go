aafce1f619a2c7d76a3923d13c03c3b531a45cb2
tikrack <tikrackcode@gmail.com>
2025-03-04 20:15:29 +0330

    feat(create): create an var and use printf

--- Hello_world/main.go
+++ Hello_world/main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var number int
+	number = 100
+
+	var float float64
+	float = float64(number)
+
+	fmt.Printf("value: %v and Type: %T \n", number, number)
+	fmt.Printf("value: %v and Type: %T \n", float, float)
 }
