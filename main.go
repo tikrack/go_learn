ae8602153643d9130a86fe88f76c4fbbf1d89958
tikrack <tikrackcode@gmail.com>
2025-10-16 10:27:04 +0330

    [Swich Case]: learned

--- main.go
+++ main.go
@@ -12,4 +12,14 @@ func main() {
 	default:
 		println("Hava is not zero 2")
 	}
+
+	switch {
+	case dama <= 0:
+		println("Hava is zero 0")
+	case dama > 10 && dama < 100:
+		println("Hava is not zero 1")
+	default:
+		println("Hava is not zero 2")
+	}
+
 }
