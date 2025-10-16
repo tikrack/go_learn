d0cdc810f19b9a79376e82c71fb9c27128998478
tikrack <tikrackcode@gmail.com>
2025-10-16 08:58:18 +0330

    [Start]: start using string methods

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
 func main() {
-	var a = "Hello World"
-	b := "Hello World"
-	var c, d = 10, 20
-	const test string = "Test"
-	println(a, b, c, d, test)
 
-	//Const is the fast because its can be see in compile time and its writed last time
 }
