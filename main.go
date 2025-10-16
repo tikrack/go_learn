0a83e30a41ee3c17f27084a5db39d918fbafafd6
tikrack <tikrackcode@gmail.com>
2025-10-16 20:48:46 +0330

    [Arr]

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
 func main() {
-	var list = []string{"Hello", "How", "Are", "you"}
 
-	var i = 0
-
-	for i < 10 {
-		i++
-		println("Hello, World!")
-	}
-
-	for x := 10; x > 0; x-- {
-		println(x)
-	}
-
-	for index, item := range list {
-		if index == 2 {
-			//break
-			//continue
-		}
-		println(index, item)
-	}
 }
