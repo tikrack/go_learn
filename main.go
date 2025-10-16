bd43c196d9418b414de7c6e545ce13be362eb57f
tikrack <tikrackcode@gmail.com>
2025-10-16 09:23:42 +0330

    [Strings]: handling using strings library

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	var str string = "Hello my name is tester"
+	var splited = strings.Split(str, " ")
+	fmt.Println(strings.Join(splited, ","))
 }
