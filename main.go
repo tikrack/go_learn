10248fcf5c403370d4b7f74ee646b8179f2709c8
tikrack <tikrackcode@gmail.com>
2025-10-16 18:44:37 +0330

    For -> starting using For

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
 
 func main() {
-	var dama = 20
-	println(dama, "\n")
-
-	switch dama {
-	case 0:
-		println("Hava is zero 0")
-	case 1:
-		println("Hava is not zero 1")
-	default:
-		println("Hava is not zero 2")
-	}
-
-	switch {
-	case dama <= 0:
-		println("Hava is zero 0")
-	case dama > 10 && dama < 100:
-		println("Hava is not zero 1")
-	default:
-		println("Hava is not zero 2")
-	}
 
 }
