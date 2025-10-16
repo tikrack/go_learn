269d38046dd155a87b7384ffd1ae24f4da4ce720
tikrack <tikrackcode@gmail.com>
2025-10-16 18:50:11 +0330

    [Foreach]

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 func main() {
+	var list = []string{"Hello", "How", "Are", "you"}
+
 	var i = 0
 
 	for i < 10 {
@@ -10,5 +12,9 @@ func main() {
 
 	for x := 10; x > 0; x-- {
 		println(x)
+	}
+
+	for index, item := range list {
+		println(index, item)
 	}
 }
