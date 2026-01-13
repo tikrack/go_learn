03931023aa780b8ea877053a08ba22e4fd0e5676
tikrack <tikrackcode@gmail.com>
2026-01-13 22:08:43 +0330

    finish

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-func main() {
-	example()
+import "fmt"
+
+type Rectangle struct {
+	Width  int
+	Height int
 }
 
-func example() {
+func (rectangle Rectangle) area() int {
+	return rectangle.Width * rectangle.Height
+}
 
+func main() {
+	rect1 := Rectangle{Width: 10, Height: 20}
+
+	fmt.Println(rect1.area())
 }
