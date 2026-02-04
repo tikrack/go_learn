c755bd3dfe04ab6d5f91762cb2c8167724ffe410
tikrack <tikrackcode@gmail.com>
2026-02-04 14:32:53 +0330

    cuncurency

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-	jalali "github.com/jalaali/go-jalaali"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"sync"
 )
 
+var TodoList []string
+
 func main() {
-	println(jalali.Now().String())
+	wg := sync.WaitGroup{}
+
+	wg.Add(5)
+
+	for i := 1; i <= 5; i++ {
+		go Get("https://jsonplaceholder.typicode.com/todos/"+strconv.Itoa(i), &wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println(TodoList)
+}
+
+func Get(url string, wg *sync.WaitGroup) {
+	println(url)
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := io.ReadAll(response.Body)
+	defer response.Body.Close()
+
+	TodoList = append(TodoList, string(res))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer wg.Done()
 }
