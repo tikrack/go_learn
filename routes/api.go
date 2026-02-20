c0c0fabb9f87f76fa6f14193240e4cc20c4c324b
tikrack <tikrackcode@gmail.com>
2026-02-20 23:31:12 +0330

    adding roomian change alogorithm

--- routes/api.go
+++ routes/api.go
@@ -3,7 +3,21 @@ package routes
 import "net/http"
 
 func Api(mux *http.ServeMux) {
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
+	routes := map[string]http.HandlerFunc{}
+
+	routes["/"] = func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Hello World</h1>"))
+	}
+
+	for route, handler := range routes {
+		mux.HandleFunc(route, func(w http.ResponseWriter, r *http.Request) {
+			if exist := routes[r.URL.Path]; exist != nil {
+				handler(w, r)
+				return
+			}
+			func() {
+				w.Write([]byte("<h1>404 NotFound</h1>"))
+			}()
+		})
+	}
 }
